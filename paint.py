1734ad6065056e4843fb1ed8e6b5e83d30873d9f
Abraham Chalita <a01653879@itesm.mx>
2021-10-27 11:03:03 -0500

    Added the "yellow color" functionality

--- paint.py
+++ paint.py
@@ -23,7 +23,6 @@ def square(start, end):
 
     end_fill()
 
-
 def circle(start, end):
     "Draw circle from start to end."
     up()
@@ -73,6 +72,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('yellow'), 'Y')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
